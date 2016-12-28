d6102b105ed7a689e270397ba9b2a5e5d3a8971e
PuZZleDucK <puzzleduck@gmail.com>
2016-12-28 21:30:01 +1100

    adding ocds identifiers and revisions

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,9 @@
-# require 'mechanize'
 require 'scraperwiki'
 require 'capybara/poltergeist'
 
-Capybara.javascript_driver = :poltergeist
-@options = { js_errors: false, timeout: 1800, phantomjs_logger: StringIO.new, logger: nil, phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes'] }
-@blacklist = ["https://maxcdn.bootstrapcdn.com/", "https://www.tenders.vic.gov.au/tenders/res/" ]
-
 def store_non_duplicate record, id_field, table='data'
   if (ScraperWiki.select("* from #{table} where `#{id_field}`='#{record[id_field]}'").empty? rescue true)
-    puts "   Storing #{record[id_field]} in #{table}"
+    puts "   Storing record"
     ScraperWiki.save_sqlite(["#{id_field}"], record, table_name=table)
   else
     puts "   Skipping already saved #{table} record #{record[id_field]}"
@@ -57,8 +52,13 @@ def extract_contract_data text, contract_index
     contract_end = Date.parse("11/10/1900")
   end
   if text.include? "UNSPSC :"
-    contract_unspsc = find_between(text, "UNSPSC :", "Description")
     contract_status = find_between(text, "Status:", "UNSPSC :")
+    if text.include? "Associated with Tender:"
+      contract_unspsc = find_between(text, "UNSPSC :", "Associated with Tender:")
+      associated_tender = find_between(text, "Associated with Tender:", "Description")
+    else
+      contract_unspsc = find_between(text, "UNSPSC :", "Description")
+    end
   else
     contract_unspsc = find_between(text, "UNSPSC 1:", "Description")
     contract_unspsc += ", "
@@ -93,18 +93,18 @@ def extract_contract_data text, contract_index
   supplier_email = find_between(text, "Email Address:", "State Government of Victoria")
   agency = find_between(text, "Public Body:", "Contract Number:")
   contract_id = find_between(text, "Contract Number:","Title:")
-  { ocds_contract_id: "ocds-k4r8nn_contract_agent-#{lookup_agency_id(agency)}_con-#{sanitize_contract_number(contract_id)}_#{contract_start}",
-    agency: agency,
+  { ocds_contract_id: "ocds-k4r8nn_agent-#{lookup_agency_id(agency)}_con-#{sanitize_contract_number(contract_id)}",
+    agency_id: clean_agency_link_text(agency),
     contract_number: contract_id,
-    contract_title: find_between(text, "Title:","Type of Contract:"),
+    contract_title: find_between(text, "Title:","Type of Contract:").gsub(";",", "),
     contract_type: find_between(text, "Type of Contract:","Total Value of the Contract:"),
     contract_value: contract_value,
-    value_type_index: value_string,
+    value_type: value_type,
     contract_start: contract_start,
     contract_end: contract_end,
     contract_status: contract_status,
     contract_unspsc: contract_unspsc,
-    contract_details: find_between(text, "Description", "Agency Contact Details"),
+    contract_details: find_between(text, "Description", "Agency Contact Details").gsub("\n",". ").gsub(";",", "),
     supplier_name: supplier_name,
     supplier_abn: supplier_abn,
     supplier_acn: supplier_acn,
@@ -112,6 +112,7 @@ def extract_contract_data text, contract_index
     agency_phone: agency_phone,
     agency_email: agency_email,
     supplier_address: supplier_address,
+    associated_tender: associated_tender,
     vt_identifier: contract_index
   }
 end
@@ -144,9 +145,8 @@ def clean_agency_link_text text
 end
 
 def lookup_agency_id agency_text
-  puts "lookup #{agency_text}"
   matching_agency = get_from_store clean_agency_link_text(agency_text), "agency_name", table='agencies'
-  puts "   matching #{matching_agency}"
+#  puts "   matching #{matching_agency}"
   if matching_agency
     return matching_agency[0]["agency_index"]
   end
@@ -166,8 +166,10 @@ end
 
 
 
-
 puts ":: TendersVIC Scrape @ #{Time.now} ::"
+Capybara.javascript_driver = :poltergeist
+@options = { js_errors: false, timeout: 1800, phantomjs_logger: StringIO.new, logger: nil, phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes'] }
+@blacklist = ["https://maxcdn.bootstrapcdn.com/", "https://www.tenders.vic.gov.au/tenders/res/" ]
 
 session = prepare_session
 session.visit "https://www.tenders.vic.gov.au/tenders/contract/list.do?action=contract-view"
@@ -179,7 +181,7 @@ department_links.each do |department_link|
   check_agency_reference department_string, agency_id
   @saved_date = department_link[:href][-10..-1]
   department_indexes_to_scrape.push(agency_id)
-#  break if department_link.text.include?("Department of Education and Training") # Stop after DET DEBUG
+#  break if department_link.text.include?("Court Services Victoria") # Stop after DEP DEBUG
 end
 session.driver.quit
 
@@ -200,7 +202,7 @@ department_indexes_to_scrape.each do |department_index|
       vt_reference = contract_link["href"].to_s[59..63]
       print "."
       contract_indexes_to_scrape.push vt_reference
-        # break # stop after first contract DEBUG
+#      break # stop after first contract DEBUG
     end
     current_page = department_session.text
     department_session.driver.quit
@@ -209,40 +211,89 @@ department_indexes_to_scrape.each do |department_index|
   print "\n"
 end
 
-
 contract_indexes_to_scrape.uniq!
-
 puts "cotract indexes: #{contract_indexes_to_scrape}"
+
+def find_partial_ocds_matches(partial_ocds_id)
+  begin
+    partial_matches = ScraperWiki.select("* from data where `ocds_contract_id` LIKE '#{partial_ocds_id}%'")
+  rescue
+  end
+#  puts "partial_matches: #{partial_matches}"
+  partial_matches
+end
+
+def get_latest_revision(matching_contracts)
+  if not matching_contracts
+    nil
+  else
+    max = {"ocds_contract_id" => "ocds_contract_id-0"}
+    matching_contracts.each do |contract|
+      if contract["ocds_contract_id"].split("-").last.to_i > max["ocds_contract_id"].split("-").last.to_i
+        max = contract
+      end
+    end
+#    puts "max: #{max}"
+    max
+  end
+end
+
+def contract_updated(last_revision, contract_data)
+  if last_revision.nil? || last_revision == {"ocds_contract_id" => "ocds_contract_id-0"}
+    true
+  else
+    false
+  end
+end
+
+def get_revision_number(contract_data)
+  partial_ocds_id = contract_data[:ocds_contract_id]
+  matching_contracts = find_partial_ocds_matches partial_ocds_id
+  last_revision = get_latest_revision(matching_contracts)
+  if contract_updated(last_revision, contract_data)
+    if matching_contracts.nil?
+      "1"
+    else
+      revisions = matching_contracts.count
+      (revisions + 1).to_s
+    end
+  else
+    ""
+  end
+end
 
 contract_session = prepare_session()
 Capybara.reset_sessions!
 contract_indexes_to_scrape.to_set.each do |contract_index|
   contract_session.visit "http://www.tenders.vic.gov.au/tenders/contract/view.do?id=#{contract_index}"
   contract_data = extract_contract_data contract_session.text, contract_index
+  revision = get_revision_number contract_data 
   contract = {
-      'ocds_contract_id' => contract_data[:ocds_contract_id],
-      'vt_contract_number' => contract_data[:contract_number],
-      'vt_status_id' => contract_data[:contract_status],
-      'vt_title' => contract_data[:contract_title],
-      'vt_start_date' => contract_data[:contract_start],
-      'vt_end_date' => contract_data[:contract_end],
-      'vt_total_value' => contract_data[:contract_value],
-      'vt_agency_id' => contract_data[:agency_id],
-      'vt_contract_type_id' => contract_data[:contract_type],
-      'vt_value_type_id' => contract_data[:value_type_index],
-      'vt_unspc_id' => contract_data[:contract_unspsc],
-      'vt_contract_description' => contract_data[:contract_details],
-      'vt_agency_person' => contract_data[:agency_person],
-      'vt_agency_phone' => contract_data[:agency_phone],
-      'vt_agency_email' => contract_data[:agency_email],
-      'vt_supplier_name' => contract_data[:supplier_name],
-      'vt_supplier_abn' => contract_data[:supplier_abn],
-      'vt_supplier_acn' => contract_data[:supplier_acn],
-      'vt_supplier_address' => contract_data[:supplier_address],
-      'vt_identifier' => contract_data[:vt_identifier]
+    'ocds_contract_id' => "#{contract_data[:ocds_contract_id]}_rev-#{revision}",
+    'vt_contract_number' => contract_data[:contract_number],
+    'vt_status' => contract_data[:contract_status],
+    'vt_title' => contract_data[:contract_title],
+    'vt_start_date' => contract_data[:contract_start],
+    'vt_end_date' => contract_data[:contract_end],
+    'vt_total_value' => contract_data[:contract_value],
+    'vt_agency' => contract_data[:agency_id],
+    'vt_contract_type' => contract_data[:contract_type],
+    'vt_value_type' => contract_data[:value_type],
+    'vt_unspc' => contract_data[:contract_unspsc],
+    'vt_associated_tender' => contract_data[:associated_tender],
+    'vt_contract_description' => contract_data[:contract_details],
+    'vt_agency_person' => contract_data[:agency_person],
+    'vt_agency_phone' => contract_data[:agency_phone],
+    'vt_agency_email' => contract_data[:agency_email],
+    'vt_supplier_name' => contract_data[:supplier_name],
+    'vt_supplier_abn' => contract_data[:supplier_abn],
+    'vt_supplier_acn' => contract_data[:supplier_acn],
+    'vt_supplier_address' => contract_data[:supplier_address],
+    'vt_info_link' => "http://www.tenders.vic.gov.au/tenders/contract/view.do?id=#{contract_data[:vt_identifier]}"
   }
-  puts "Uniq: #{contract['ocds_contract_id']}"
-  store_non_duplicate contract, 'ocds_contract_id'
+  puts ":: Processing: #{contract['ocds_contract_id']}_rev-#{revision}"
+  store_non_duplicate contract, 'ocds_contract_id' unless revision == ""
+  puts "   Skipping already saved record" unless not revision == ""
 end
 contract_session.driver.quit
 puts ":: Completed Scraping @ #{Time.now} ::\n"

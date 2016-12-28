5cce60292cfa9729798f4bc14afd4273b2ce970e
PuZZleDucK <puzzleduck@gmail.com>
2016-12-28 15:24:11 +1100

    remove custom data types and fix text matching

--- scraper.rb
+++ scraper.rb
@@ -27,169 +27,87 @@ def get_from_store id_value, id_field, table='data'
   ScraperWiki.select("* from #{table} where `#{id_field}`='#{id_value}'")
 end
 
-
-
-
-ContractType = []
-def ContractType.where(x)
-  ContractType
-end
-ContractValueType = []
-def ContractValueType.where(x)
-  ContractValueType
-end
-def ContractValueType.all()
-  ContractValueType
-end
-ContractStatus = []
-def ContractStatus.where(x)
-  ContractStatus
-end
-def ContractStatus.all()
-  ContractStatus
-end
-Unspsc = []
-def Unspsc.all()
-  Unspsc
-end
-
-def lookup_contract_unspsc text
-  Unspsc.all.each do |unspsc_category|
-    if text.include?(unspsc_category.unspsc_name)
-      return unspsc_category.unspsc_code
-    end
-  end
-  0
-end
-
-
-  def lookup_contract_status text
-    con_status = ContractStatus.where(name: text).first
-    if con_status
-      con_status.id
-    else
-      0
-    end
-  end
-
-  def lookup_contract_status_name status_id
-    status_type = ContractStatus.where(id: status_id).first
-    if status_type
-      status_type.name
-    else
-      ""
-    end
-  end
-
-
-  def lookup_value_type text
-    value_type = 0
-    ContractValueType.all.each do |value_type|
-      if text.include? value_type.type_description
-        value_type = value_type.id
-        break
-      end
-    end
-    value_type
-  end
-
-
-  def lookup_contract_type_name type_id
-    con_type = ContractType.where(id: type_id).first
-    if con_type
-      con_type.name
-    else
-      ""
-    end
-  end
-
-  def lookup_contract_type text
-    con_type = ContractType.where(name: text).first
-    if con_type
-      con_type.id
-    else
-      0
-    end
-  end
-
-
-def find_between(text, pre_string, post_string)
+def find_between(text, pre_string, post_string, print=true)
   matches = text.match(/#{pre_string}(.*?)#{post_string}/)
   if matches && matches.length > 1
     matches[1].strip
   else
     puts "Match failed!"
-    puts "nothing between '#{pre_string}' & '#{post_string}'"
-    puts "in #{text}"
+    if print then puts "nothing between '#{pre_string}' & '#{post_string}' in '#{text}'" end
     ""
   end
 end
 
 
-  def extract_contract_data text, contract_index, print=false
-    gov_entity = find_between(text, "Public Body:", "Contract Number:")
-    gov_entity_contract_numb = find_between(text, "Contract Number:","Title:")
-    contract_title = find_between(text, "Title:","Type of Contract:")
-    contract_type = find_between(text, "Type of Contract:","Total Value of the Contract:")
-    value_string = find_between(text, "Total Value of the Contract:","Start Date:")
-    value_type = find_between(value_string,"(",")")
-    contract_value = (value_string.gsub(",","").gsub("$","").to_f).to_i
-    begin
-      contract_start = Date.parse(find_between(text, "Start Date:","Expiry Date:"))
-    rescue
-      contract_start = Date.parse("11/10/1900")
-    end
-    begin
-      contract_end = Date.parse(find_between(text, "Expiry Date:","Status:"))
-    rescue
-      contract_end = Date.parse("11/10/1900")
-    end
-    contract_status = find_between(text, "Status:", "UNSPSC :")
-    begin
-      contract_unspsc1 = find_between(text, "UNSPSC :", "Description")
-    rescue
-      contract_unspsc1 = find_between(text, "UNSPSC 1:", "Description")
-      contract_unspsc2 = find_between(text, "UNSPSC 2:", "Description")
-    end
-    contract_details = find_between(text, "Description", "Agency Contact Details")
-    agency_person = find_between(text, "Contact Person:", "Contact for factual information purposes")
-    agency_phone = find_between(text, "Contact Number:", "Email Address:") # check for contact # existance
-    agency_email = find_between(text, "Email Address:", "Supplier Information")
-    supplier_name = find_between(text, "Supplier Name:", "ABN:")
-    supplier_abn = find_between(text, "ABN:", "ACN:")
-    if text.include?("DUNS #:")
-      supplier_acn = find_between(text, "ACN:", "DUNS #:")
-    else
-      supplier_acn = find_between(text, "ACN:", "Address:")
-    end
-    chunk = find_between(text, "Email Address:", "State:")
-    street = find_between(chunk, "Address:", "Suburb:")
-    suburb = find_between(text, "Suburb:", "State:")
-    state = find_between(text, "State:", "Postcode:")
-    post_code = find_between(text, "Postcode:", "Email Address:")
-    supplier_address = "#{street}, #{suburb}, #{state} #{post_code}"
-    supplier_email = find_between(text, "Email Address:", "State Government of Victoria")
-    { department_id: lookup_agency_id(gov_entity),
-      contract_number: gov_entity_contract_numb,
-      contract_title: contract_title,
-      contract_type: lookup_contract_type(contract_type),
-      contract_value: contract_value,
-      value_type_index: lookup_value_type(value_string),
-      contract_start: contract_start,
-      contract_end: contract_end,
-      contract_status: lookup_contract_status(contract_status),
-      contract_unspsc: lookup_contract_unspsc(contract_unspsc1),
-      contract_details: contract_details,
-      supplier_name: supplier_name,
-      supplier_abn: supplier_abn,
-      supplier_acn: supplier_acn,
-      agency_person: agency_person,
-      agency_phone: agency_phone,
-      agency_email: agency_email,
-      supplier_address: supplier_address,
-      vt_identifier: contract_index
-    }
+def extract_contract_data text, contract_index
+  value_string = find_between(text, "Total Value of the Contract:","Start Date:")
+  value_type = find_between(value_string,"(",")")
+  contract_value = (value_string.gsub(",","").gsub("$","").to_f).to_i
+  begin
+    contract_start = Date.parse(find_between(text, "Start Date:","Expiry Date:"))
+  rescue
+    contract_start = Date.parse("11/10/1900")
   end
+  begin
+    contract_end = Date.parse(find_between(text, "Expiry Date:","Status:"))
+  rescue
+    contract_end = Date.parse("11/10/1900")
+  end
+  begin
+    contract_unspsc = find_between(text, "UNSPSC :", "Description")
+  rescue
+    contract_unspsc = find_between(text, "UNSPSC 1:", "Description")
+    contract_unspsc += ", "
+    contract_unspsc += find_between(text, "UNSPSC 2:", "Description")
+  end
+  agency_chunk = find_between(text, "Agency Contact Details", "Supplier Information")
+  agency_chunk += "Supplier Information"
+  agency_person = find_between(agency_chunk, "Contact Person:", "Contact for factual information purposes")
+  agency_phone = find_between(agency_chunk, "Contact Number:", "Email Address:", false)
+  if agency_phone.nil?
+    agency_phone = find_between(agency_chunk, "Mobile:", "Email Address:")
+  end
+  if agency_chunk.include? "Email Address:"
+    agency_email = find_between(agency_chunk, "Email Address:", "Supplier Information")
+  else
+    agency_email = "No email contact provided"
+  end
+  supplier_name = find_between(text, "Supplier Name:", "ABN:")
+  supplier_abn = find_between(text, "ABN:", "ACN:")
+  if text.include?("DUNS #:")
+    supplier_acn = find_between(text, "ACN:", "DUNS #:")
+  else
+    supplier_acn = find_between(text, "ACN:", "Address:")
+  end
+  chunk = find_between(text, "Supplier Information", "State:")
+  street = find_between(chunk, "Address:", "Suburb:")
+  suburb = find_between(text, "Suburb:", "State:")
+  state = find_between(text, "State:", "Postcode:")
+  post_code = find_between(text, "Postcode:", "Email Address:")
+  supplier_address = "#{street}, #{suburb}, #{state} #{post_code}"
+  supplier_email = find_between(text, "Email Address:", "State Government of Victoria")
+
+  { department_id: lookup_agency_id(find_between(text, "Public Body:", "Contract Number:")),
+    contract_number: find_between(text, "Contract Number:","Title:"),
+    contract_title: find_between(text, "Title:","Type of Contract:"),
+    contract_type: find_between(text, "Type of Contract:","Total Value of the Contract:"),
+    contract_value: contract_value,
+    value_type_index: value_string,
+    contract_start: contract_start,
+    contract_end: contract_end,
+    contract_status: find_between(text, "Status:", "UNSPSC :"),
+    contract_unspsc: contract_unspsc,
+    contract_details: find_between(text, "Description", "Agency Contact Details"),
+    supplier_name: supplier_name,
+    supplier_abn: supplier_abn,
+    supplier_acn: supplier_acn,
+    agency_person: agency_person,
+    agency_phone: agency_phone,
+    agency_email: agency_email,
+    supplier_address: supplier_address,
+    vt_identifier: contract_index
+  }
+end
 
 
 
@@ -205,7 +123,7 @@ end
 def check_agency_reference agency_string, agency_index
   is_present = check_store agency_index, "agency_index", table='data'
   if not is_present
-    puts "Dynamicaly creating '#{agency_string}' with ref #{agency_index}"
+    puts "Checking '#{agency_string}' with ref #{agency_index}"
     store_non_duplicate ({"agency_index" => "#{agency_index}", "agency_name" => "#{agency_string}"}), "agency_index", table='agencies'
   end
 end
@@ -226,7 +144,7 @@ end
 def lookup_agency_name agency_id
   matching_agency = get_from_store agency_id, "agency_index", table='agencies'
   if matching_agency
-    matching_agency.name
+    matching_agency[0]["agency_name"]
   else
     puts "could not find agency id '#{agency_id}'"
     ""
@@ -236,7 +154,7 @@ end
 
 
 
-print "\n ∵ TendersVIC Scrape @ #{Time.now} ∵\n"
+puts ":: TendersVIC Scrape @ #{Time.now} ::"
 
 session = prepare_session
 session.visit "https://www.tenders.vic.gov.au/tenders/contract/list.do?action=contract-view"
@@ -252,34 +170,31 @@ department_links.each do |department_link|
 end
 session.driver.quit
 
-  contract_indexes_to_scrape = []
-  department_indexes_to_scrape.each do |department_index|
-    print "Agency: #{department_index}: "
-    page_number = 1
-    previous_page = ""
-    current_page = "not blank"
-    while previous_page != current_page
-      previous_page = current_page
-      department_session = prepare_session
-      department_url = "https://www.tenders.vic.gov.au/tenders/contract/list.do?showSearch=false&action=contract-search-submit&issuingBusinessId=#{department_index}&issuingBusinessIdForSort=#{department_index}&pageNum=#{page_number}&awardDateFromString=#{@saved_date}"
-      department_session.visit department_url
-      contract_links = department_session.find_all "a#MSG2"
-      print "\n   Page #{page_number}: "
-      contract_links.each do |contract_link|
-        vt_reference = contract_link["href"].to_s[59..63]
-        print "."
-        contract_indexes_to_scrape.push vt_reference
-          # break # stop after first contract DEBUG
-      end
-      current_page = department_session.text
-      department_session.driver.quit
-      page_number += 1
+contract_indexes_to_scrape = []
+department_indexes_to_scrape.each do |department_index|
+  print ":: #{lookup_agency_name department_index} ::"
+  page_number = 1
+  previous_page = ""
+  current_page = "not blank"
+  while previous_page != current_page
+    previous_page = current_page
+    department_session = prepare_session
+    department_url = "https://www.tenders.vic.gov.au/tenders/contract/list.do?showSearch=false&action=contract-search-submit&issuingBusinessId=#{department_index}&issuingBusinessIdForSort=#{department_index}&pageNum=#{page_number}&awardDateFromString=#{@saved_date}"
+    department_session.visit department_url
+    contract_links = department_session.find_all "a#MSG2"
+    print "\n   Page #{page_number}: "
+    contract_links.each do |contract_link|
+      vt_reference = contract_link["href"].to_s[59..63]
+      print "."
+      contract_indexes_to_scrape.push vt_reference
+        # break # stop after first contract DEBUG
     end
-    print "\n"
+    current_page = department_session.text
+    department_session.driver.quit
+    page_number += 1
   end
-
-
-
+  print "\n"
+end
 
 
 
@@ -290,7 +205,7 @@ contract_session = prepare_session()
 Capybara.reset_sessions!
 contract_indexes_to_scrape.to_set.each do |contract_index|
   contract_session.visit "http://www.tenders.vic.gov.au/tenders/contract/view.do?id=#{contract_index}"
-  contract_data = extract_contract_data(contract_session.text, contract_index, print)
+  contract_data = extract_contract_data contract_session.text, contract_index
   contract = {
       'vt_contract_number' => contract_data[:contract_number],
       'vt_status_id' => contract_data[:contract_status],
@@ -315,5 +230,5 @@ contract_indexes_to_scrape.to_set.each do |contract_index|
   store_non_duplicate contract, 'vt_contract_number'
 end
 contract_session.driver.quit
-print "\n ∴ Completed Scraping @ #{Time.now} ∴\n"
+puts ":: Completed Scraping @ #{Time.now} ::\n"
 
